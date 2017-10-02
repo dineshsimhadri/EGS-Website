49d3971712421ea78a8524e3a600cb5a8e330cbf
dineshsimhadri <dinesh.simhadri99@gmail.com>
2017-10-01 22:38:47 -0700

    Change in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -55,6 +55,8 @@ class Home extends Component {
                 </div>
                 <h3>Staffing</h3>
                 <p>Put the right people on the right job through end-to-end Professional Services.</p>
+                <p>Minimize time to market and ensure faster ROI through cutting edge technology services.</p>
+                <p>To do the end-to-end Professional Services.</p>                
                 <button type="button" className="learn-btn btn btn-primary hidden-xs">Learn more</button>
               </Link>
             </div>
